df95f3dccde3f30ddcc31bc0c19abc8489ef149e
prenitha10 <prenitha1004@gmail.com>
2024-10-21 20:30:32 +0530

    Time: 0 ms (100%), Space: 17.2 MB (65.44%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        string=s.split()
+        for i in range (len(string)):
+            string[i]=string[i][::-1]
+        return " ".join(string)
+
+        
+        
