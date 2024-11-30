c641eb84dbf1f2cb3ffa9bace4768536359164e6
prenitha10 <prenitha1004@gmail.com>
2024-11-30 20:35:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (11.61%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public int[] plusOne(int[] digits) 
+    {
+        int n=digits.length-1;
+        for(int i=n;i>=0;i--)
+        {
+            if(digits[i]<9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        int[] arr=new int[n+2];
+        arr[0]=1;
+        return arr;
+    }
+
+}
