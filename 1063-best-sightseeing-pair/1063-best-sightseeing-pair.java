942b997c8090b928982276ed66e1c9f8c6aa2a35
prenitha10 <prenitha1004@gmail.com>
2024-12-28 06:53:45 +0530

    Time: 2 ms (100%), Space: 53.7 MB (32.97%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
@@ -1,12 +1,18 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
         int max=0;
-        int PrevVal=values[0];
+        int PrevVal=values[0]+0;
         int n=values.length;
         for(int i=1;i<n;i++)
         {   
-            max=Math.max(max,PrevVal+values[i]-i);
-            PrevVal=Math.max(PrevVal,values[i]+i);
+            if(PrevVal+values[i]-i>max)
+            {
+                max=PrevVal+values[i]-i;
+            }
+            if(values[i]+i>PrevVal)
+            {
+                PrevVal=values[i]+i;
+            }
         }  
         return max;      
     }
