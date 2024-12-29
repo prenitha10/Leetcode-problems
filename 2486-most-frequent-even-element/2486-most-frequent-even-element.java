0613dd73e345d0157437d67b0637382c808f86a5
prenitha10 <prenitha1004@gmail.com>
2024-12-29 21:35:13 +0530

    Time: 34 ms (17.38%), Space: 45.6 MB (6.74%) - LeetHub

--- 2486-most-frequent-even-element/2486-most-frequent-even-element.java
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.java
@@ -1,27 +1,27 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        Map<Integer, Integer> hmap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                hmap.put(nums[i], hmap.getOrDefault(nums[i], 0) + 1);
+        Map<Integer,Integer> hmap=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]%2==0)
+            {
+                hmap.put(nums[i],hmap.getOrDefault(nums[i],0)+1);
             }
         }
-
-        int maxKey = -1;
-        int maxVal = -1;
-
-        List<Integer> list = new ArrayList<>(hmap.keySet());
-
-        Collections.sort(list, Collections.reverseOrder());
-
-        for (int ele : list) {
-            int val = hmap.get(ele);
-            if (maxKey <= val) {
-                maxVal = ele;
-                maxKey = val;
+        int maxVal=0;
+        int maxKey=0;
+        ArrayList<Integer> arr=new ArrayList<>(hmap.keySet());
+        Collections.sort(arr,Collections.reverseOrder());
+        for(int ele:arr)
+        {
+            int val=hmap.get(ele);
+            if(val>=maxVal)
+            {
+                maxVal=val;
+                maxKey=ele;
             }
         }
-
-        return maxVal;
+        if(maxVal==0) return -1;
+            return maxKey;
     }
 }
