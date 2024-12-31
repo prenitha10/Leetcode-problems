800ceba9937b650c4862655a46a28bd47913fe38
prenitha10 <prenitha1004@gmail.com>
2024-12-31 09:45:54 +0530

    Time: 0 ms (100%), Space: 40.1 MB (81.28%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -1,9 +1,21 @@
 class Solution {
-    public int tribonacci(int n) {
+    public int tribonacci(int n) 
+    {
+        HashMap<Integer,Integer> hmap=new HashMap<>();
+        return rec(n,hmap);        
+    }
+    public int rec(int n,HashMap<Integer,Integer> hmap)
+    {
+        int val;
+        
         if(n==0)
             return 0;
         if(n<=2)
             return 1;
-        return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);              
+        if(hmap.containsKey(n))
+            return hmap.get(n);
+        val=rec(n-1,hmap)+rec(n-2,hmap)+rec(n-3,hmap);
+        hmap.put(n,val);
+        return val;
     }
 }
