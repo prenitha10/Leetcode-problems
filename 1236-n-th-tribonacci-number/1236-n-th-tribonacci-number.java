ecfbbac0d876d9404c66f8d92ae68600e770cd5e
prenitha10 <prenitha1004@gmail.com>
2024-12-31 09:33:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0)
+            return 0;
+        if(n<=2)
+            return 1;
+        return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);              
+    }
+}
