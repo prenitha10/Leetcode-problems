2143d482010a00ec585ce08b4271aa1d0da2f395
Prenitha S P <prenitha1004@gmail.com>
2025-03-26 05:34:38 +0530

    Time: 8 ms (2.18%), Space: 42.2 MB (6.73%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        HashMap<String,Integer> hmap=new HashMap<>();
-        return rec(m,n,hmap);        
-    }
-    int rec(int m,int n,HashMap<String,Integer> hmap)
+    public int uniquePaths(int m, int n) 
     {
-        String key=m+ "," +n;
+        HashMap<String,Integer> hmap=new HashMap<>();
+        return backtrack(m,n,hmap);        
+    }
+    int backtrack(int m,int n,HashMap<String,Integer> hmap)
+    {
+        String key=m+" "+n;
         if(hmap.containsKey(key))
-        {
             return hmap.get(key);
-        }
-        if(m==1 || n==1)
+        if(m==1||n==1)
             return 1;
-        int val=rec(m-1,n,hmap)+rec(m,n-1,hmap);
+        int val= backtrack(m-1,n,hmap)+ backtrack(m,n-1,hmap);
         hmap.put(key,val);
         return val;
     }
+    
 }
