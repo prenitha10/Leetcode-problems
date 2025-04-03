5d7661590d540678cbaff78f0632871c4e7c02f5
Prenitha S P <prenitha1004@gmail.com>
2025-04-03 19:48:43 +0530

    Time: 1 ms (99.56%), Space: 57.4 MB (13.36%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum=0;
         int max=Integer.MIN_VALUE;
+        int sum=0;
         for(int i=0;i<nums.length;i++)
         {
             sum+=nums[i];
@@ -9,7 +9,7 @@ class Solution {
                 max=Math.max(max,sum);
             if(sum<0)
                 sum=0;
-        }        
+        }
         return max;
     }
 }
