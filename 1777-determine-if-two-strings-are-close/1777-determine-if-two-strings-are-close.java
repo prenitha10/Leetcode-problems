5b284b340ac7289a5f659916e2ab81f0fb65f607
Prenitha S P <prenitha1004@gmail.com>
2025-04-16 11:25:53 +0530

    Time: 10 ms (84.97%), Space: 45.6 MB (82.78%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -1,33 +1,30 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
+        int[] freq=new int[26];
+        int[] freq1=new int[26];
         if(word1.length()!=word2.length())
             return false;
-        char[] ch=word1.toCharArray();
-        char[] ch2=word2.toCharArray();
-        HashMap<Character,Integer> hmap=new HashMap<>();
-        HashMap<Character,Integer> hmap2=new HashMap<>();
-
-        ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
-
         for(int i=0;i<word1.length();i++)
         {
-            hmap.put(ch[i],hmap.getOrDefault(ch[i],0)+1);
-            hmap2.put(ch2[i],hmap2.getOrDefault(ch2[i],0)+1);
+            freq[word1.charAt(i)-'a']++;
         }
-        for(int ele:hmap.values())
-            list.add(ele);
-        Collections.sort(list);
-        for(int ele:hmap2.values())
-            list2.add(ele);
-        Collections.sort(list2);
         for(int i=0;i<word1.length();i++)
         {
-            if(!word2.contains(String.valueOf(ch[i])) )
+            freq1[word2.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if((freq[i]>0 && freq1[i]==0)||(freq[i]>0 && freq1[i]==0))
                 return false;
         }
-        if(!list.equals(list2))
-            return false;
+        Arrays.sort(freq);
+        Arrays.sort(freq1);
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]!=freq1[i])
+                return false;
+        }
         return true;
+        
     }
 }
